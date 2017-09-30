15df1972656f7d2072cc4da56e9c6f5c94818b46
kimjihoon <kimjihoon@218.38.137.27>
2017-09-30 21:48:26 +0900

    repeat

--- src/jihun/calendar/Month.java
+++ src/jihun/calendar/Month.java
@@ -3,20 +3,26 @@ package jihun.calendar;
 import java.util.Scanner;
 
 public class Month {
-	public static int maxdays(int month) {
+	public int maxdays(int month) {
 		int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
 		
 		return days[month-1]; 
 	}
 	public static void main(String[] args) {
+		Month maxmonth = new Month();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
-		int day = maxdays(month);
+		System.out.println("반복횟수를 입력하세여");
+		int num = scanner.nextInt();
 		
-		System.out.println(month+"월은 "+day+"까지 있습니다.");
+		for(int i=0;i<num;i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			System.out.println(month+"월은 "+maxmonth.maxdays(month)+"까지 있습니다.");
+			
+		}
+		System.out.println("BYE~");
 		
-		
+				
 				
 		scanner.close();
 	}
