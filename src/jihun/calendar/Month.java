25d0e6737aa41ec29b67c270013183141adf62b0
kimjihoon <kimjihoon@218.38.137.27>
2017-09-30 21:14:40 +0900

    input month using method

--- src/jihun/calendar/Month.java
+++ src/jihun/calendar/Month.java
@@ -3,13 +3,19 @@ package jihun.calendar;
 import java.util.Scanner;
 
 public class Month {
+	public static int maxdays(int month) {
+		int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		return days[month-1]; 
+	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
-		int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+		int day = maxdays(month);
 		
-		System.out.println(month+ "월은" +days[month-1]+"일 까지입니다. ");
+		System.out.println(month+"월은 "+day+"까지 있습니다.");
+		
 		
 				
 		scanner.close();
